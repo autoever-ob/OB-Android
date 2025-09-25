efef04b5bbcdfc19c22ac3146fc3ff4a619193c0
Hojip-Kim <rlaghwlq12@naver.com>
2025-09-25 11:29:33 +0900

    feat : 핫픽스

--- app/src/main/java/com/nobody/campick/fragments/HomeFragment.kt
+++ app/src/main/java/com/nobody/campick/fragments/HomeFragment.kt
@@ -1,26 +1,15 @@
 package com.nobody.campick.fragments
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
-import androidx.navigation.compose.rememberNavController
-import com.nobody.campick.R
-import com.nobody.campick.activities.VehicleDetailActivity
-import com.nobody.campick.databinding.FragmentHomeBinding
 import com.nobody.campick.ui.theme.CampickTheme
-import com.nobody.campick.viewmodels.LoginViewModel
-import com.nobody.campick.viewmodels.SignupFlowViewModel
-import com.nobody.campick.views.FavoritesView
 import com.nobody.campick.views.Home
-import com.nobody.campick.views.Login
-import com.nobody.campick.views.SignupFlow
 
 class HomeFragment : Fragment() {
-    @SuppressLint("ViewModelConstructorInComposable")
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -28,22 +17,9 @@ class HomeFragment : Fragment() {
     ): View {
         return ComposeView(requireContext()).apply {
             setContent {
-                val navController = rememberNavController()
-//                val viewModel = LoginViewModel()
-                val viewModel = SignupFlowViewModel()
-
                 CampickTheme {
-//                    Home()
-                    SignupFlow(
-                        navController, viewModel,
-                        onComplete = {
-                            parentFragmentManager.beginTransaction()
-                                .replace(R.id.fragmentContainer, LoginFragment())
-                                .commit()
-                        }
-                    )
+                    Home()
                 }
-
             }
         }
     }
